258b06cf5ec7c02366c6bc4f50179d38629e73f6
NorhaneCh <kn_cherif@esi.dz>
2023-12-24 19:23:50 +0100

    init project

---
+++ payemnt_process/src/main/java/tp2/paymentApplication.java
@@ -0,0 +1,8 @@
+package tp2;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+@ProcessApplication("Payment App")
+public class paymentApplication extends ServletProcessApplication {
+// empty implementation
+}
