44f98a5d04f2e8223b9d609945f3a8e939428c6d
NorhaneCh <kn_cherif@esi.dz>
2024-01-02 22:31:59 +0100

    fixing smth

--- payemnt_process/src/main/java/tp2/paymentApplication.java
+++
@@ -1,8 +0,0 @@
-package tp2;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-@ProcessApplication("Payment App")
-public class paymentApplication extends ServletProcessApplication {
-// empty implementation
-}
